940e0c6d93b127f52baae93d1e16871bc61fb609
Golluméo <p.mauriello54@gmail.com>
2025-05-07 11:47:48 +0200

    feat(contract): add method to SpecDiscoveryContract interface
    Introduced `find` method with optional path parameter to discover specs.

--- src/Application/Contract/SpecDiscoveryContract.php
+++ src/Application/Contract/SpecDiscoveryContract.php
@@ -4,4 +4,10 @@ declare(strict_types=1);
 
 namespace Gollumeo\Verrastra\Application\Contract;
 
-interface SpecDiscoveryContract {}
+interface SpecDiscoveryContract
+{
+    /**
+     * @return class-string[]
+     */
+    public function find(string $path = 'specs'): array;
+}
