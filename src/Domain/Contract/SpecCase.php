0606278dccb63aab8bee6ddd42cc9c4b99dd7747
Golluméo <p.mauriello54@gmail.com>
2025-05-07 11:48:05 +0200

    refactor(Domain): simplify SpecCase interface
    Removed the `find` method from the `SpecCase` interface, leaving it empty.

--- src/Domain/Contract/SpecCase.php
+++ src/Domain/Contract/SpecCase.php
@@ -4,10 +4,4 @@ declare(strict_types=1);
 
 namespace Gollumeo\Verrastra\Domain\Contract;
 
-interface SpecCase
-{
-    /**
-     * @return class-string[]
-     */
-    public function find(string $path = 'specs'): array;
-}
+interface SpecCase {}
